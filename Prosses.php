9910fa213205c0649bd1d4157ec58da136110c40
Navod-web <104830696@student.swin.edu.au>
2025-05-09 11:01:53 +1000

    my pass

--- Prosses.php
+++ Prosses.php
@@ -2,10 +2,10 @@
     <h2>Login</h2>
     <form action="process.php" method="POST">
         <label for="username">Username:</label>
-        <input type="text" name="username" id="admin" required><br><br>
+        <input type="text" name="admin" id="admin" required><br><br>
 
         <label for="password">Password:</label>
-        <input type="text" name="password" id="kavindu" required><br><br>
+        <input type="text" name="kavindu" id="kavindu" required><br><br>
 
         <!-- Hidden field for user token -->
         <input type="hidden" name="user_token" value="F1234567"><br><br>
